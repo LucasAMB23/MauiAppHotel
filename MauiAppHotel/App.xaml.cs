3d933f0460e5ed130a77f909109228a9e758cac5
Lucas <lsoaresbarbosa20@gmail.com>
2026-05-24 19:20:35 -0300

    Adicionado classe Quarto para usar no Picker

--- MauiAppHotel/App.xaml.cs
+++ MauiAppHotel/App.xaml.cs
@@ -1,9 +1,35 @@
-﻿using Microsoft.Extensions.DependencyInjection;
+﻿using MauiAppHotel.Models;
+using Microsoft.Extensions.DependencyInjection;
 
 namespace MauiAppHotel
 {
     public partial class App : Application
     {
+        public List<Quarto> lista_quartos = new List<Quarto>()
+        {
+            new Quarto()
+            {
+                Descricao = "Suíte Super Luxo",
+                ValorDiariaAdulto = 120.0,
+                ValorDiariaCrianca = 60.0
+            },
+            new Quarto()
+            {
+                Descricao = "Suíte Luxo",
+                ValorDiariaAdulto = 100.0,
+                ValorDiariaCrianca = 50.0
+            },
+            new Quarto()
+            {
+                Descricao = "Suíte Econômica",
+                ValorDiariaAdulto = 70.0,
+                ValorDiariaCrianca = 35.0
+            }
+
+
+
+        };
+
         public App()
         {
              InitializeComponent();
--- MauiAppHotel/Views/PedidoHospedagem.xaml.cs
+++ MauiAppHotel/Views/PedidoHospedagem.xaml.cs
@@ -2,9 +2,19 @@ namespace MauiAppHotel.Views;
 
 public partial class PedidoHospedagem : ContentPage
 {
+    App PropriedadesApp;
 	public PedidoHospedagem()
 	{
 		InitializeComponent();
+
+        PropriedadesApp = (App)Application.Current;
+
+        picker_quarto.ItemsSource = PropriedadesApp.lista_quartos;
+
+        dt_checkin.MinimumDate = DateTime.Now;
+        dt_checkin.MaximumDate = new DateTime(DateTime.Today.Year, DateTime.Now.Month + 1, DateTime.Now.Day);
+
+        
 	}
 
     private void Button_Clicked(object sender, EventArgs e)
